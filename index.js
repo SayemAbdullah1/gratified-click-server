babd0c550dccb0faf51b37b9f284b6ce6f74d504
SayemAbdullah1 <dxsayem0@gmail.com>
2022-11-09 14:38:01 +0600

    add to db and find all data from mongoDb

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -7,6 +9,27 @@ const port = process.env.PORT || 5000;
 //middleWare
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1q1hbsc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollection = client.db('gratifiedClick').collection('services')
+
+        app.get('/services', async(req, res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err))
 
 app.get('/', (req, res)=>{
     res.send('gratified click server is running')
